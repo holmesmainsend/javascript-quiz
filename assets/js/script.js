32286ad6c509003a4cdc222cd094d08b14714969
Mona Lisa <holmesmainsend@gmail.com>
2021-10-14 21:01:37 -0400

    began adding timer functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,29 @@
 const beginEl = document.querySelector(".begin");
 const leaderboardEl = document.querySelector(".leaderboard");
 const retireEl = document.querySelector(".retire");
+var timerDisplayEl = document.querySelector(".timerDisplay");
+var timerValue = 10;
 
 // Event listeners
-beginEl.addEventListener("click", function() {
-    console.log("Begin");
-});
+beginEl.addEventListener("click", countdown, {});
+
 
 leaderboardEl.addEventListener("click", function() {
-    console.log("Leaderboard");
+    console.log("leaderboard");
 });
 
 retireEl.addEventListener("click", function () {
-    console.log("Retire");
+    close();
 });
+
+// Functions
+function countdown() {
+    setInterval(tickTock, 1000);
+};
+
+function tickTock() {
+    if (timerValue > 0) {
+    timerValue--;
+    timerDisplayEl.innerHTML = timerValue;
+    } else (timerValue.innerText = "Game Over");
+};
