414230dcd0beb97d75d530a6353a757222d8e3b9
Mona Lisa <holmesmainsend@gmail.com>
2021-10-16 12:24:51 -0400

    fixed arrays, buttons, timer lower bound

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+// TODO: clear out option buttons after each question answered
+// TODO: do not repeat timer function after each new question
+// TODO: subtract time with wrong answer
+// TODO: add "Game Over" screen once timer runs out or all questions are answered
+// TODO: save timer score to localStorage with user initials input (do not accept empty string)
+// TODO: retrieve scores from localStorage for leaderboard page
+// TODO: add polish/CSS
+
 // DOM values
 const beginEl = document.querySelector(".begin");
 const leaderboardEl = document.querySelector(".leaderboard");
@@ -5,6 +13,7 @@ const retireEl = document.querySelector(".retire");
 const mainEl = document.querySelector("main");
 const titlerEl = document.querySelector(".titler");
 var timerDisplayEl = document.querySelector(".timerDisplay");
+var currentQuestion = 0
 // var timerValue = 5;
 
 
@@ -13,94 +22,64 @@ const questionArray = [
   {
     title:
       "Which of the following Math methods will always round x down to the nearest integer?",
-    option1: "Math.round(x)",
-    option2: "Math.floor(x)",
-    option3: "Math.trunc(x)",
-    option4: "Math.ceil(x)",
-    answer: 2,
+    options: ["Math.round(x)", "Math.floor(x)", "Math.trunc(x)", "Math.ceil(x)"],
+    answer: "Math.floor(x)"
   },
 
   {
     title: "How many distinct values are available to the Boolean data type?",
-    option1: "1",
-    option2: "Depends on the context",
-    option3: "2",
-    option4: "0",
-    answer: 3,
+    options: ["1", "Depends on the context", "2", "0"],
+    answer: "2",
   },
 
   {
     title: "In JavaScript, a triple equals sign (===) means:",
-    option1: "Equal in both value and type",
-    option2: "Not equal",
-    option3: "And",
-    option4: "Equal in value but not type",
-    answer: 1,
+    options: ["Equal in both value and type", "Not equal", "And", "Equal in value but not type"],
+    answer: "Equal in both value and type",
   },
 
   {
     title: "What approximate percentage of websites today use JavaScript?",
-    option1: "97%",
-    option2: "67.4%",
-    option3: "89%",
-    option4: "34%",
-    answer: 1,
+    options: ["97%", "67.4%", "89%", "34%"],
+    answer: "97%",
   },
 
   {
     title: "With what do you separate methods in an object?",
-    option1: "A period (.)",
-    option2: "A pair of brackets ([])",
-    option3: "A dollar sign ($)",
-    option4: "A comma (,)",
-    answer: 4,
+    options: ["A period (.)", "A pair of brackets ([])", "A dollar sign ($)", "A comma (,)"],
+    answer: "A comma (,)",
   },
 
   {
     title: "Which of the following is an example of an array?",
-    option1: "var client = 'Branson Automotive'",
-    option2: "let x = 20.4",
-    option3: "const titleEl = document.querySelector('.title')",
-    option4: "const database = ['MySQL', 'Oracle Database', 'MongoDB']",
-    answer: 4,
+    options: ["var client = 'Branson Automotive'", "let x = 20.4", "const titleEl = document.querySelector('.title')", "const database = ['MySQL', 'Oracle Database', 'MongoDB']"],
+    answer: "const database = ['MySQL', 'Oracle Database', 'MongoDB']",
   },
 
   {
     title:
       "Your long-lost brother Benson is trapped in a never-ending loop that has paralyzed him with fear. Which command will free him from his looping nightmare?",
-    option1: "return;",
-    option2: "break;",
-    option3: "end;",
-    option4: "switch;",
-    answer: 2,
+    options: ["return;", "break;", "end;", "switch;"],
+    answer: "break;",
   },
 
   {
     title:
       "You want to subtract 1 from the var 'tasksToDo'. How could you go about this as quickly as possible?",
-    option1: "tasksToDo minus 1",
-    option2: "tasksToDo.takeOne",
-    option3: "tasksToDo--",
-    option4: "tasksToDo.spliced",
-    answer: 3,
+    options: ["tasksToDo minus 1", "tasksToDo.takeOne", "tasksToDo--", "tasksToDo.spliced"],
+    answer: "tasksToDo--",
   },
 
   {
     title: "Which extension do JavaScript files typically save with?",
-    option1: ".jav",
-    option2: ".javas",
-    option3: ".jvscrt",
-    option4: ".js",
-    answer: 4,
+    options: [".jav", ".javas", ".jvscrt", ".js"],
+    answer: ".js",
   },
 
   {
     title: "What does DOM stand for?",
-    option1: "Derived Oracle Main",
-    option2: "Document Object Model",
-    option3: "Doltish Oaf Mentality",
-    option4: "Daimer-Operand Mode",
-    answer: 2,
+    options: ["Derived Oracle Main", "Document Object Model", "Doltish Oaf Mentality", "Daimer-Operand Mode"],
+    answer: "Document Object Model",
   },
 ];
 
@@ -129,34 +108,57 @@ function countdown() {
 function startQuiz() {
   countdown();
   beginRemover();
-  for (var i = 0; i < questionArray.length; i++) {
-    titlerEl.innerText = questionArray[i].title;
-    var option1Button = document.createElement("button");
-    var option2Button = document.createElement("button");
-    var option3Button = document.createElement("button");
-    var option4Button = document.createElement("button");
-    option1Button.textContent = questionArray[i].option1;
-    option2Button.textContent = questionArray[i].option2;
-    option3Button.textContent = questionArray[i].option3;
-    option4Button.textContent = questionArray[i].option4;
-    mainEl.appendChild(option1Button);
-    mainEl.appendChild(option2Button);
-    mainEl.appendChild(option3Button);
-    mainEl.appendChild(option4Button);
-
-    option1Button.addEventListener("click", console.log("This is option1"));
-    option2Button.addEventListener("click", console.log("This is option2"));
-    option3Button.addEventListener("click", console.log("This is option3"));
-    option4Button.addEventListener("click", console.log("This is option4"));
-
-
-    // if (response == questionArray[i].answer) {
-    //   alert("Correct");
-    // } else {
-    //   alert("WRONG");
-    // }
-  }
+  var currentPrompt = questionArray[currentQuestion];
+  currentPrompt.options.forEach(function(choice, i){
+    titlerEl.innerText = questionArray[currentQuestion].title;
+    var optionButton = document.createElement("button");
+    optionButton.innerText = choice;
+    mainEl.appendChild(optionButton);
+    optionButton.addEventListener("click", function() {
+      verifyChoice(optionButton);
+    })
+  })
 }
+
+function verifyChoice(optionButton) {
+  console.log(questionArray[currentQuestion].answer)
+  console.log(optionButton.innerText)
+  if (questionArray[currentQuestion].answer === optionButton.innerText) {
+    console.log("Correct answer");
+    currentQuestion++
+    startQuiz();
+} else {
+  currentQuestion++
+  startQuiz();
+}
+}
+  // for (var i = 0; i < questionArray.length; i++) {
+  //   titlerEl.innerText = questionArray[i].title;
+  //   var option1Button = document.createElement("button");
+  //   var option2Button = document.createElement("button");
+  //   var option3Button = document.createElement("button");
+  //   var option4Button = document.createElement("button");
+  //   option1Button.textContent = questionArray[i].option1;
+  //   option2Button.textContent = questionArray[i].option2;
+  //   option3Button.textContent = questionArray[i].option3;
+  //   option4Button.textContent = questionArray[i].option4;
+  //   mainEl.appendChild(option1Button);
+  //   mainEl.appendChild(option2Button);
+  //   mainEl.appendChild(option3Button);
+  //   mainEl.appendChild(option4Button);
+
+  //   option1Button.addEventListener("click", console.log("This is option1"));
+  //   option2Button.addEventListener("click", console.log("This is option2"));
+  //   option3Button.addEventListener("click", console.log("This is option3"));
+  //   option4Button.addEventListener("click", console.log("This is option4"));
+
+
+  //   // if (response == questionArray[i].answer) {
+  //   //   alert("Correct");
+  //   // } else {
+  //   //   alert("WRONG");
+  //   // }
+  // }
 
 
 // Event listeners
